3bad3ea365cd323d61a913e4999e68fe5c64c3a8
Kara Kelley <kelley.15@gmail.com>
2018-02-09 15:00:07 -0600

    Configure for travis

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tictactoe "0.1.0-SNAPSHOT"
+(defproject tictactoe "tictactoe"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
