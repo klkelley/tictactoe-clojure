5c07ac3dff2e0036bf12a44fec31b0e13013879f
Kara Kelley <kelley.15@gmail.com>
2018-02-11 12:28:13 -0600

    Add first test for user interface

---
+++ test/tictactoe/user_interface_test.clj
@@ -0,0 +1,7 @@
+(ns tictactoe.user-interface-test
+  (:require [clojure.test :refer :all]
+           [tictactoe.user-interface :refer :all])) 
+
+(deftest class-setup
+  ( testing "idk"
+    (is (= 1 1))))
