37bf8dc4ea139b5a8296715c967e39b2ad92006b
Kara Kelley <kelley.15@gmail.com>
2018-02-12 16:24:52 -0600

    Add game messages

---
+++ src/tictactoe/game_messages.clj
@@ -0,0 +1,19 @@
+(ns tictactoe.game-messages)
+
+(defn welcome []
+  "Welcome to Tic Tac Toe!\n")
+
+(defn game-type []
+  "Pick a game type: \n")
+
+(defn board-type [] 
+  "What size board would you like to play? \n")
+
+(defn player-one-marker []
+  "Please pick player 1's marker (A-Z)\n")
+
+(defn player-two-marker []
+  "Please pick player 2's marker (A-Z)\n")
+
+(defn game-choices []
+	"You've chosen: \n")
