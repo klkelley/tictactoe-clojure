34026cd3a91da09d5111d3084daa7cdfc2e2df8f
Kara Kelley <kelley.15@gmail.com>
2018-02-14 08:43:21 -0600

    Add function to loop and print key value pairs

--- src/tictactoe/user_interface.clj
+++ src/tictactoe/user_interface.clj
@@ -1,7 +1,13 @@
 (ns tictactoe.user-interface)
 
-(defn print-message [message]
-  (println (str message)))
+(defn print-message
+  ([message]
+  	(println message))
+  ([message supplementing-info]
+  	(println message "=>" supplementing-info)))
 
 (defn get-input []
   (read-line))
+
+(defn loop-and-print [options]
+	(doseq [[k v] (map identity options)] (print-message (name k) v)))
