27452e031eba2e2276576052698d534eeda2d594
Kara Kelley <kelley.15@gmail.com>
2018-02-14 08:42:44 -0600

    Changed game options for displaying

--- src/tictactoe/game_runner.clj
+++ src/tictactoe/game_runner.clj
@@ -2,10 +2,10 @@
   (:require [tictactoe.game-menu :as game-menu]))
 
 (def game-types
-  {:1 "1. Human vs. Human" :2 "2. Human vs. Easy Computer"})
+  {:1 "Human vs. Human" :2 "Human vs. Easy Computer"})
 
 (def board-sizes
-	{:1 "1. 3x3" :2 "2. 4x4"})
+	{:1 "3x3" :2 "4x4"})
 
 
 (defn start []
