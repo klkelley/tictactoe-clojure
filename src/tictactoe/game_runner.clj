3231260723535f3ba39d7b4425d8eb7ea7604990
Kara Kelley <kelley.15@gmail.com>
2018-02-14 09:26:51 -0600

    Fix indentation

--- src/tictactoe/game_runner.clj
+++ src/tictactoe/game_runner.clj
@@ -2,7 +2,7 @@
   (:require [tictactoe.game-menu :as game-menu]))
 
 (def game-types
-  {:1 "Human vs. Human" :2 "Human vs. Easy Computer"})
+	{:1 "Human vs. Human" :2 "Human vs. Easy Computer"})
 
 (def board-sizes
 	{:1 "3x3" :2 "4x4"})
@@ -10,4 +10,4 @@
 
 (defn start []
 	(let [game-options (game-menu/menu game-types board-sizes)]
-	 game-options))
+	game-options))
