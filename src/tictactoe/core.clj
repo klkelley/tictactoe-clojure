1a0236e2151d79fe435dd03b6bcf5c275e222361
Kara Kelley <kelley.15@gmail.com>
2018-02-14 10:01:03 -0600

    Add gen-class directive

--- src/tictactoe/core.clj
+++ src/tictactoe/core.clj
@@ -1,5 +1,6 @@
 (ns tictactoe.core
- (:require [tictactoe.game-runner :as game-runner]))
+  (:gen-class)
+  (:require [tictactoe.game-runner :as game-runner]))
 
 (defn -main []
   (game-runner/start))
