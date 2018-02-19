dd0e420eb0fcb96905af3d44854915245c48ad0a
Kara Kelley <kelley.15@gmail.com>
2018-02-19 12:55:22 -0600

    Update tests to play full games

--- test/tictactoe/game_runner_test.clj
+++ test/tictactoe/game_runner_test.clj
@@ -1,6 +1,7 @@
 (ns tictactoe.game-runner-test
   (:require [clojure.test :refer [deftest is testing]]
-            [tictactoe.game-runner :as game-runner]))
+            [tictactoe.game-runner :as game-runner]
+            [clojure.string :as string]))
 
 (defn make-input
   [coll]
@@ -10,64 +11,45 @@
   [& args]
   (apply str (interpose "\n" args)))
 
-(deftest test-start-with-all-valid-input
-  (testing "Loops through all game options without errors when valid input is received"
-    (is (= (lines "Welcome to Tic Tac Toe!"
-                  ""
-                  "Pick a game type:"
-                  "1 => Human vs. Human\n2 => Human vs. Easy Computer"
-                  "What size board would you like to play?"
-                  "1 => 3x3"
-                  "2 => 4x4"
-                  "Please pick player 1's marker (A-Z)"
-                  "Please pick player 2's marker (A-Z)"
-                  "You've chosen:"
-                  "game => Human vs. Human"
-                  "board => 4x4"
-                  "player1 => A"
-                  "player2 => B\n")
-           (with-out-str ""
-             (with-in-str (make-input '("1" "2" "a" "b")) (game-runner/start)))))))
-
-
-(deftest test-invalid-game-choice
-  (testing "Error message is displayed when invalid input for game type is received"
-  (is (= (lines "Welcome to Tic Tac Toe!"
-                ""
-                "Pick a game type:"
-                "1 => Human vs. Human\n2 => Human vs. Easy Computer"
-                "Invalid. Please pick an available option."
-                "What size board would you like to play?"
-                "1 => 3x3"
-                "2 => 4x4"
-                "Please pick player 1's marker (A-Z)"
-                "Please pick player 2's marker (A-Z)"
-                "You've chosen:"
-                "game => Human vs. Human"
-                "board => 4x4"
-                "player1 => A"
-                "player2 => B\n")
+(deftest test-game-play-human-v-human
+  (testing "Player one wins the game"
+  (is (= true (string/includes?
          (with-out-str ""
-           (with-in-str (make-input '("4" "1" "2" "a" "B")) (game-runner/start)))))))
+           (with-in-str (make-input '("1" "1" "X" "O" "0" "5" "1" "4" "2")) (game-runner/start))) "Player X wins!" )))))
+
+(deftest test-game-human-v-human-player2-wins
+  (testing "Player two wins the game"
+  (is (= true (string/includes?
+         (with-out-str ""
+           (with-in-str (make-input '("1" "1" "X" "O" "1" "4" "2" "8" "7" "0")) (game-runner/start))) "Player O wins!" )))))
+
+(deftest test-4x4-game-human-v-human-player1-wins
+  (testing "Player two wins the game"
+  (is (= true (string/includes?
+         (with-out-str ""
+           (with-in-str (make-input '("1" "2" "X" "O" "0" "4" "1" "8" "2" "13" "3")) (game-runner/start))) "Player X wins!" )))))
+
+(deftest invalid-marker-choice
+  (testing "User will be prompted for different input if input is invalid"
+  (is (= true (string/includes?
+         (with-out-str ""
+           (with-in-str (make-input '("1" "1" "x" "5" "p" "0" "4" "1" "3" "2")) (game-runner/start))) "Invalid. Please pick another marker." )))))
 
 
+(deftest invalid-menu-choice
+  (testing "User must choose an available menu option"
+  (is (= true (string/includes?
+         (with-out-str ""
+           (with-in-str (make-input '("3" "1" "1" "x" "p" "0" "4" "1" "3" "2")) (game-runner/start))) "Invalid. Please pick an available option." )))))
 
-(deftest test-invalid-marker-choice
-  (testing "Error message is displayed when invalid marker choice is received"
-    (is (= (lines "Welcome to Tic Tac Toe!"
-                  ""
-                  "Pick a game type:"
-                  "1 => Human vs. Human\n2 => Human vs. Easy Computer"
-                  "What size board would you like to play?"
-                  "1 => 3x3"
-                  "2 => 4x4"
-                  "Please pick player 1's marker (A-Z)"
-                  "Please pick player 2's marker (A-Z)"
-                  "Invalid. Please pick another marker."
-                  "You've chosen:"
-                  "game => Human vs. Human"
-                  "board => 4x4"
-                  "player1 => A"
-                  "player2 => B\n")
-           (with-out-str ""
-             (with-in-str (make-input '("1" "2" "A" "a" "B")) (game-runner/start)))))))
+(deftest play-tie-game
+  (testing "Game can be played to a tie"
+  (is (= true (string/includes?
+         (with-out-str ""
+           (with-in-str (make-input '("1" "1" "x" "p" "0" "4" "1" "2" "6" "3" "5" "7" "8")) (game-runner/start))) "Its a tie!" )))))
+
+(deftest test-cell-choice-validation
+  (testing "User will continue to be prompted for valid input and available spaces"
+  (is (= true (string/includes?
+         (with-out-str ""
+           (with-in-str (make-input '("1" "1" "x" "p" "0" "0" "f" "4" "1" "2" "6" "3" "5" "7" "8")) (game-runner/start))) "Its a tie!" )))))
