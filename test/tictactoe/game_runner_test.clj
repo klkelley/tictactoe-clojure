00bb71f6c776ba1e4bc00a7fb1157aaf58e95880
Kara Kelley <kelley.15@gmail.com>
2018-02-19 13:07:08 -0600

    Remove unused helper function for tests

--- test/tictactoe/game_runner_test.clj
+++ test/tictactoe/game_runner_test.clj
@@ -7,49 +7,51 @@
   [coll]
   (apply str (interleave coll (repeat "\n"))))
 
-(defn lines
-  [& args]
-  (apply str (interpose "\n" args)))
-
 (deftest test-game-play-human-v-human
-  (testing "Player one wins the game"
+  (testing "Player one wins the game on a 3x3 board"
   (is (= true (string/includes?
          (with-out-str ""
-           (with-in-str (make-input '("1" "1" "X" "O" "0" "5" "1" "4" "2")) (game-runner/start))) "Player X wins!" )))))
+           (with-in-str (make-input '("1" "1" "X" "O" "0" "5" "1" "4" "2")) 
+            (game-runner/start))) "Player X wins!" )))))
 
 (deftest test-game-human-v-human-player2-wins
-  (testing "Player two wins the game"
+  (testing "Player two wins the game on a 3x3 board"
   (is (= true (string/includes?
          (with-out-str ""
-           (with-in-str (make-input '("1" "1" "X" "O" "1" "4" "2" "8" "7" "0")) (game-runner/start))) "Player O wins!" )))))
+           (with-in-str (make-input '("1" "1" "X" "O" "1" "4" "2" "8" "7" "0")) 
+            (game-runner/start))) "Player O wins!" )))))
 
 (deftest test-4x4-game-human-v-human-player1-wins
-  (testing "Player two wins the game"
+  (testing "Player one wins the game on a 4x4 board"
   (is (= true (string/includes?
          (with-out-str ""
-           (with-in-str (make-input '("1" "2" "X" "O" "0" "4" "1" "8" "2" "13" "3")) (game-runner/start))) "Player X wins!" )))))
+           (with-in-str (make-input '("1" "2" "X" "O" "0" "4" "1" "8" "2" "13" "3")) 
+            (game-runner/start))) "Player X wins!" )))))
 
 (deftest invalid-marker-choice
-  (testing "User will be prompted for different input if input is invalid"
+  (testing "User will be prompted for different marker if choice is invalid"
   (is (= true (string/includes?
          (with-out-str ""
-           (with-in-str (make-input '("1" "1" "x" "5" "p" "0" "4" "1" "3" "2")) (game-runner/start))) "Invalid. Please pick another marker." )))))
-
+           (with-in-str (make-input '("1" "1" "x" "5" "p" "0" "4" "1" "3" "2")) 
+            (game-runner/start))) "Invalid. Please pick another marker." )))))
 
 (deftest invalid-menu-choice
   (testing "User must choose an available menu option"
   (is (= true (string/includes?
          (with-out-str ""
-           (with-in-str (make-input '("3" "1" "1" "x" "p" "0" "4" "1" "3" "2")) (game-runner/start))) "Invalid. Please pick an available option." )))))
+           (with-in-str (make-input '("3" "1" "1" "x" "p" "0" "4" "1" "3" "2"))
+            (game-runner/start))) "Invalid. Please pick an available option." )))))
 
 (deftest play-tie-game
   (testing "Game can be played to a tie"
   (is (= true (string/includes?
          (with-out-str ""
-           (with-in-str (make-input '("1" "1" "x" "p" "0" "4" "1" "2" "6" "3" "5" "7" "8")) (game-runner/start))) "Its a tie!" )))))
+           (with-in-str (make-input '("1" "1" "x" "p" "0" "4" "1" "2" "6" "3" "5" "7" "8")) 
+            (game-runner/start))) "Its a tie!" )))))
 
 (deftest test-cell-choice-validation
   (testing "User will continue to be prompted for valid input and available spaces"
   (is (= true (string/includes?
          (with-out-str ""
-           (with-in-str (make-input '("1" "1" "x" "p" "0" "0" "f" "4" "1" "2" "6" "3" "5" "7" "8")) (game-runner/start))) "Its a tie!" )))))
+           (with-in-str (make-input '("1" "1" "x" "p" "0" "0" "f" "4" "1" "2" "6" "3" "5" "7" "8")) 
+            (game-runner/start))) "Its a tie!" )))))
